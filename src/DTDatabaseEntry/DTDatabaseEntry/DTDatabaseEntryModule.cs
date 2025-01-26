18b302467854e07f593fc27e109b4d42103842f8
Dmytro Trofimchuk <dmytro.trofimchuk@acupower.co.uk>
2025-01-26 14:23:41 +0200

    feature/2 Implemet action to identify DB type

--- src/DTDatabaseEntry/DTDatabaseEntry/DTDatabaseEntryModule.cs
+++ src/DTDatabaseEntry/DTDatabaseEntry/DTDatabaseEntryModule.cs
@@ -5,6 +5,11 @@ namespace DTDatabaseEntry
 {
     public class DTDatabaseEntryModule : Module
     {
+        /// <summary>
+        /// Configures dependency injection by registering the <see cref="DTDatabase"/> 
+        /// implementation for the <see cref="IDTDatabase"/> interface.
+        /// </summary>
+        /// <param name="builder">The container builder used to register dependencies.</param>
         protected override void Load(ContainerBuilder builder)
         {
             base.Load(builder);
--- src/DTDatabaseEntry/DTDatabaseEntry/Services/IDTDatabase.cs
+++ src/DTDatabaseEntry/DTDatabaseEntry/Services/IDTDatabase.cs
@@ -2,6 +2,10 @@ namespace DTDatabaseEntry.Services
 {
     public interface IDTDatabase
     {
+        /// <summary>
+        /// Retrieves the type of database being used by the Acumatica instance.
+        /// </summary>
+        /// <returns>A string representing the database type.</returns>
         string GetDBType();
     }
 }
